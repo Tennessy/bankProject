ded88c01a5a673a8ababdfd5097b0e7f463ab68e
Alexislavie <lavie.alexis@gmail.com>
2012-12-01 20:49:37 +0100

    Ajout des fichiers oubliés !

---
+++ admin/admin_html/form_addUser.php
@@ -0,0 +1,6 @@
+<fieldset>
+		<legend>Login</legend>
+		<form method="post" action="admin.php" name="form_addUser" id="addUser">
+			
+		</form>
+</fieldset>
---
+++ root_html/form_logout.php
@@ -0,0 +1,7 @@
+<fieldset>
+		<legend>Logout</legend>
+		<form method="post" action="index.php" name="form_logout" id="logout">
+			<p> <input type="submit" value="Se déconnecter" name="post_logout" />
+			</p>
+		</form>
+</fieldset>
